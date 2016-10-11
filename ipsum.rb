790fcd996af35ab0186abbfe82a34c5b22659fc3
mpanuszewska@gmai.com <marta.panuszewska@lunarlogic.io>
2016-10-11 10:11:13 +0200

    added variations on text length

--- ipsum.rb
+++ ipsum.rb
@@ -2,9 +2,16 @@ require 'sinatra'
 require 'marky_markov'
 
 get '/' do
+	@text_length =  params[:"text-length"]
 	markov = MarkyMarkov::TemporaryDictionary.new
 	markov.parse_file "file.txt"
-	@generated = markov.generate_n_sentences 5
+	if @text_length == "short" then
+		@generated = markov.generate_n_sentences 1
+	elsif @text_length == "medium" then
+		@generated = markov.generate_n_sentences 5
+	else 
+		@generated = markov.generate_n_sentences 25
+	end
  	erb :index
 end
 
