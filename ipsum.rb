3e003bb6c3f5e9d0d37548b7e8a1cc9cb639fca6
mpanuszewska@gmai.com <marta.panuszewska@lunarlogic.io>
2016-10-12 13:16:48 +0200

    breaking long text into paragraphs and setting up default value

--- ipsum.rb
+++ ipsum.rb
@@ -10,12 +10,16 @@ get '/' do
 
   if @text_length == "short" then
     @generated = markov.generate_n_sentences 5
-  elsif @text_length == "medium" then
-    @generated = markov.generate_n_sentences 15
+  elsif @text_length == "long" then
+    @generated = markov.generate_n_sentences 10
+    @generated += "\n\n"
+    @generated += markov.generate_n_sentences 10
+    @generated += "\n\n"
+    @generated += markov.generate_n_sentences 10
   else 
-    @generated = markov.generate_n_sentences 25
+    @generated = markov.generate_n_sentences 15
   end
-  
+
   if request.xhr? then
     @generated 
   else 
