75521c432d357b006a3d1aba0bcff57e72e8842c
mpanuszewska@gmai.com <marta.panuszewska@lunarlogic.io>
2016-10-12 10:42:32 +0200

    Changed tabs for spaces and number of generated sentences

--- ipsum.rb
+++ ipsum.rb
@@ -3,26 +3,27 @@ require 'marky_markov'
 require 'pry' #testing purposes
 
 get '/' do
-	markov = MarkyMarkov::TemporaryDictionary.new
-	markov.parse_file "jargon.txt"
-	markov.parse_file "wiki.txt"
-	@text_length =  params[:"text-length"]
-	puts params
-	if @text_length == "short" then
-		@generated = markov.generate_n_sentences 1
-	elsif @text_length == "medium" then
-		@generated = markov.generate_n_sentences 5
-	else 
-		@generated = markov.generate_n_sentences 25
-	end
-	if request.xhr? then
-		@generated 
-	else 
-		erb :index
-	end
-	
+  markov = MarkyMarkov::TemporaryDictionary.new
+  markov.parse_file "jargon.txt"
+  markov.parse_file "wiki.txt"
+  @text_length =  params[:"text-length"]
+
+  if @text_length == "short" then
+    @generated = markov.generate_n_sentences 5
+  elsif @text_length == "medium" then
+    @generated = markov.generate_n_sentences 15
+  else 
+    @generated = markov.generate_n_sentences 25
+  end
+  
+  if request.xhr? then
+    @generated 
+  else 
+    erb :index
+  end
+  
 end
 
 get '/style.css' do
-	scss :"../stylesheets/style"
+  scss :"../stylesheets/style"
 end
