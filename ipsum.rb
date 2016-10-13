2fbeff77d8e13ae9ea97ca46fba1d57cd09a487b
mpanuszewska@gmai.com <marta.panuszewska@lunarlogic.io>
2016-10-13 12:10:41 +0200

    whitespace bug fixed, some changes to txt files

--- ipsum.rb
+++ ipsum.rb
@@ -3,21 +3,21 @@ require 'marky_markov'
 require 'pry' #testing purposes
 
 get '/' do
-  markov = MarkyMarkov::TemporaryDictionary.new
-  markov.parse_file "jargon.txt"
-  markov.parse_file "wiki.txt"
+  load_markov_dictionary
   @text_length =  params[:"text-length"]
+  @text_length ||= "medium"
 
   if @text_length == "short" then
-    @generated = markov.generate_n_sentences 5
-  elsif @text_length == "long" then
-    @generated = markov.generate_n_sentences 10
-    @generated += "\n\n"
-    @generated += markov.generate_n_sentences 10
-    @generated += "\n\n"
-    @generated += markov.generate_n_sentences 10
+    @generated = @markov.generate_n_sentences 5
+  elsif @text_length == "medium" then
+    @generated = @markov.generate_n_sentences 15
   else 
-    @generated = markov.generate_n_sentences 15
+    @generated = @markov.generate_n_sentences 10
+    @generated += "\n\n"
+    @generated += @markov.generate_n_sentences 10
+    @generated += "\n\n"
+    @generated += @markov.generate_n_sentences 10
+
   end
 
   if request.xhr? then
@@ -30,4 +30,14 @@ end
 
 get '/style.css' do
   scss :"../stylesheets/style"
+end
+
+
+def load_markov_dictionary
+  @markov = MarkyMarkov::Dictionary.new('dictionary')
+  unless File.exist?('dictionary.mmd')
+    @markov.parse_file "jargon.txt"
+    @markov.parse_file "wiki.txt"
+    @markov.save_dictionary! # Saves the modified dictionary/creates one if it didn't exist.
+  end
 end
